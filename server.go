e29740a31c9478154642328de27b4fe0b576e4da
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-08-17 09:52:39 +0700

    add get data by id

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -16,20 +18,33 @@ func NewHttp() *fiber.App {
 	students := app.Group("/students", Auth())
 	students.Get("/",
 		Permission("students", PERMISSION_READ),
-		Permission("students", PERMISSION_CREATE),
 		func(c *fiber.Ctx) error {
 			return c.JSON(fiber.Map{
 				"message": "all students",
-				"data":    []bool{},
+				"data":    []struct{}{},
 			})
 		},
 	)
+
 	students.Post("/",
 		Permission("students", PERMISSION_CREATE),
 		func(c *fiber.Ctx) error {
 			return c.JSON(fiber.Map{
 				"message": "student created",
-				"data":    []bool{},
+				"data":    struct{}{},
+			})
+		},
+	)
+
+	students.Get("/:id",
+		Permission("students", PERMISSION_READ),
+		Permission("students", PERMISSION_CREATE),
+		Permission("students", PERMISSION_UPDATE),
+		Permission("students", PERMISSION_DELETE),
+		func(c *fiber.Ctx) error {
+			return c.JSON(fiber.Map{
+				"message": fmt.Sprintf("student with id %s", c.Params("id", "0")),
+				"data":    struct{}{},
 			})
 		},
 	)
